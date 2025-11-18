f428bc9fee1d08e060d67159a8d9ed32d68bee4b
hardeepaks <hardeepaks2000@gmail.com>
2025-11-18 12:07:27 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/policies', require('./routes/policies'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/vehicle', require('./routes/vehicle'));
+
+app.get('/',(req,res)=>res.send('Backend running'));
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>console.log("Running",PORT));
